c22eecefdcb02c8b53621778cbc0cb3108773121
anish <anishkarki695@gmail.com>
2024-01-28 18:39:19 +0545

    Serving static files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 //1st middlewares
 app.use(morgan("dev"));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
