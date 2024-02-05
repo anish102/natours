0775edc9c2a8a7f2e5e1e57daa4eef399917c615
anish102 <anishkarki695@gmail.com>
2024-02-05 16:46:36 +0545

    Importing development data

--- models/tourModel.js
+++ models/tourModel.js
@@ -43,7 +43,7 @@ const tourSchema = new mongoose.Schema({
   },
   imageCover: {
     type: String,
-    required: [true, `A tour must have a cover image!`],
+    required: [true, `A tour must consists a cover image!`],
   },
   images: [String],
   createdAt: {
